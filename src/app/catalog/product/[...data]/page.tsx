22431e9561db98f91b9ce77639b121772135f320
Neander de Souza <neander.faria@hotmail.com>
2023-10-27 09:34:43 -0300

    Fundamentos App Router - React Server Components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,11 +1,22 @@
+'use client'
+
 interface ProductProps {
   params: {
     data: string[]
   }
 }
 
+// Server Components => A gente não USA JavaScript no lado do cliente
+// Client Components => O JavaScript é enviado ao navegador (cliente)
+
 export default function Product({ params }: ProductProps) {
   const [productId, size, color] = params.data
+
+  console.log(params)
+
+  function addToCart() {
+    console.log('Adicionou ao carrinho')
+  }
 
   return (
     <div>
@@ -14,6 +25,8 @@ export default function Product({ params }: ProductProps) {
       <p>Size: {size}</p>
 
       <p>Color: {color}</p>
+
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
     </div>
   )
 }
